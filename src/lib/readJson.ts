7ef4b139ed61076a8c5b85feb574608ca2d31c66
ar4rpon <hsgw28tech@gmail.com>
2024-01-22 23:26:42 +0900

    feat:#1 jsonファイルを読み込む処理の実装

---
+++ src/lib/readJson.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+
+// JSONファイルを読み込む関数
+export async function readJsonFile(filePath: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      try {
+        const jsonObject = JSON.parse(data);
+        resolve(jsonObject); // JSONをJavaScriptオブジェクトに変換して解決する
+      } catch (parseError) {
+        reject(parseError);
+      }
+    });
+  });
+}
